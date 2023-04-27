0ae00758462809cf029843b49ddfe23ba345b92b
cedMac <mace.cedric@live.fr>
2023-04-27 23:41:16 +0200

    gestion app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClientContainerComponent } from './client-container/client-container.component';
+import { ClientListComponent } from './client-container/client-list/client-list.component';
+import { ClientDetailComponent } from './client-container/client-detail/client-detail.component';
+import { ROUTING } from './app.routing';
+import { ClientRowComponent } from './client-container/client-list/client-row/client-row.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ClientContainerComponent,
+    ClientListComponent,
+    ClientDetailComponent,
+    ClientRowComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, ROUTING, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
