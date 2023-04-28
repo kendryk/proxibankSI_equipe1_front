491616a3c3363dea750c62d2bbef9440e0b3f401
cedMac <mace.cedric@live.fr>
2023-04-28 08:51:53 +0200

    affichage de chaque client

--- src/app/client-container/client-list/client-row/client-row.component.ts
+++ src/app/client-container/client-list/client-row/client-row.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Client } from 'src/app/models/Client';
 
 @Component({
   selector: 'app-client-row',
   templateUrl: './client-row.component.html',
-  styleUrls: ['./client-row.component.css']
+  styleUrls: ['./client-row.component.css'],
 })
 export class ClientRowComponent {
+  @Input() oneClient!: Client;
+
+
+  ngOnInit() {
+    console.log(this.oneClient);
+  }
 
 }
