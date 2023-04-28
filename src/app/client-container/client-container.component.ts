1c2f11ed0647d337370453e5a1cd7c3c6c6209b5
cedMac <mace.cedric@live.fr>
2023-04-28 08:50:59 +0200

    modification component client container

--- src/app/client-container/client-container.component.ts
+++ src/app/client-container/client-container.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { ListClientsConseillerService } from '../services/list-clients-conseiller.service';
-import { Client } from '../models/Client';
-import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-client-container',
@@ -10,24 +8,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ClientContainerComponent {
   
-  allClients!: Client[];
   constructor(
-    private serviceListClient: ListClientsConseillerService,
-    private route: ActivatedRoute
+   
     ){};
 
 
-  ngOnInit() {
-    const advisorId = this.route.snapshot.params['id']; // Récupère l'ID de l'URL
-    this.serviceListClient.getListClientByAdvisorId(advisorId).subscribe({
-      next: (result: Client[]) => {
-        this.allClients = result;
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
-  }
+ 
 }
-
-//implement ondestroy
