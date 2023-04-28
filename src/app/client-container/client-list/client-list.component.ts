6dbdb784dbaa61758895a5cd3839b2288dd84a24
cedMac <mace.cedric@live.fr>
2023-04-28 08:51:26 +0200

    ajout de la liste client

--- src/app/client-container/client-list/client-list.component.ts
+++ src/app/client-container/client-list/client-list.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { ListClientsConseillerService } from 'src/app/services/list-clients-conseiller.service';
+import { Client } from 'src/app/models/Client';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-client-list',
   templateUrl: './client-list.component.html',
-  styleUrls: ['./client-list.component.css']
+  styleUrls: ['./client-list.component.css'],
 })
 export class ClientListComponent {
+  allClients: Client[] = [];
+  constructor(
+    private serviceListClient: ListClientsConseillerService,
+    private route: ActivatedRoute
+  ) {}
 
+  ngOnInit() {
+    const advisorId = this.route.snapshot.params['id']; // Récupère l'ID de l'URL
+    this.serviceListClient.getListClientByAdvisorId(advisorId).subscribe({
+      next: (result: Client[]) => {
+        this.allClients = result;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
+//implement ondestroy
