9b36bbf55dfd3ad7f3027cd7a3f264ced13415b7
cedMac <mace.cedric@live.fr>
2023-04-30 00:45:33 +0200

    lien formulaire edition

--- src/app/client-container/client-detail/client-detail.component.ts
+++ src/app/client-container/client-detail/client-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { ActivatedRoute, Routes } from '@angular/router';
+import { ActivatedRoute, Router, Routes } from '@angular/router';
 import { Client } from 'src/app/models/Client';
 import { ListClientsConseillerService } from 'src/app/services/list-clients-conseiller.service';
 
@@ -10,19 +10,22 @@ import { ListClientsConseillerService } from 'src/app/services/list-clients-cons
 })
 export class ClientDetailComponent {
   @Input() clientSelected!: Client;
+  advisorId: string | undefined;
+  // selectedClient!: Client;
 
-  selectedClient!: Client;
-
-  constructor(
-    private clientService: ListClientsConseillerService,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOninit() {
-    const advisorId = this.route.snapshot.params['id'];
-    console.log(this.clientService.getListClientByAdvisorId(advisorId));
+    this.advisorId = this.route.snapshot.params['id'];
   }
-  recupererSelectedClient(client: Client) {
-    this.selectedClient = client;
+  // recupererSelectedClient(client: Client) {
+  //   this.selectedClient = client;
+  // }
+
+  goToEditClient(clientId: number) {
+    this.advisorId = this.route.snapshot.params['id'];
+    this.router.navigateByUrl(
+      `/advisor/${this.advisorId}/updateClient/${clientId}`
+    );
   }
 }
