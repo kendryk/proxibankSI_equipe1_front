6d837d90f33809c1200a2be99d61074b0ac4c2ec
cedMac <mace.cedric@live.fr>
2023-04-29 11:13:16 +0200

    mise en place des routes pour le formulaire

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,15 @@
 import { RouterModule, Routes } from "@angular/router";
 import { ClientContainerComponent } from "./client-container/client-container.component";
+import { FormClientComponent } from "./form-client/form-client.component";
 
 const myRoutes: Routes = [
+  // Todo revoir les nom des id
   { path: 'advisor/:id/clients', component: ClientContainerComponent },
+  { path: 'advisor/:advisorId/newClient', component: FormClientComponent },
+  {
+    path: 'advisor/:advisorId/updateClient/:clientId',
+    component: FormClientComponent,
+  },
 ];
 
 
