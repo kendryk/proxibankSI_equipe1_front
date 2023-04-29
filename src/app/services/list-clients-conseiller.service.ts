8ce28cadb25ebb180858c99603451534514807ce
cedMac <mace.cedric@live.fr>
2023-04-29 11:12:39 +0200

    modification du service list conseiller

--- src/app/services/list-clients-conseiller.service.ts
+++ src/app/services/list-clients-conseiller.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Client } from '../models/Client';
 import { HttpClient } from '@angular/common/http';
 
@@ -7,12 +7,39 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class ListClientsConseillerService {
- 
   constructor(private http: HttpClient) {}
 
   link = 'http://localhost:8080/advisors';
 
   getListClientByAdvisorId(id: string): Observable<Client[]> {
     return this.http.get<Client[]>(`${this.link}/${id}/listClient`);
+  }
+
+  getClientByIdByAdvisorId(
+    clientId: string,
+    advisorId: string
+  ): Observable<Client> {
+    return this.http
+      .get<Client[]>(`${this.link}/${advisorId}/listClient`)
+      .pipe(
+        map((clients: Client[]) =>
+          clients.find((c: Client) => c.id === Number(clientId))
+        )
+      );
+  }
+
+  addClientByAdvisorId(id: string, newClient: Client): Observable<Client> {
+    return this.http.post<Client>(`${this.link}/${id}/addClient`, newClient);
+  }
+
+  updateClientOfAdvisorById(
+    advisorId: string,
+    newClient: Client,
+    clientId: string
+  ): Observable<Client> {
+    return this.http.put<Client>(
+      `${this.link}/${advisorId}/updateClient/${clientId}`,
+      newClient
+    );
   }
 }
