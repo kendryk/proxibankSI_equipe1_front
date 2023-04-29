a3d74ad925d5e99c7fcf9d61ab4dd9957c8dcde6
Dequé <carolinedeque@gmail.com>
2023-04-29 17:02:20 +0200

    Ajout du model compte

---
+++ src/app/models/Account.ts
@@ -0,0 +1,11 @@
+export class Account {
+  constructor(
+    public accountNumberAsString: string,
+    public accountStatus: string,
+    public balance: number,
+    public created: Date,
+    public id: number,
+    public number: string,
+    public rate: number
+  ) {}
+}
