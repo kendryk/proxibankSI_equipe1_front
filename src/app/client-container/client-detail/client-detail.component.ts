ac9cbced5c9ec5108e2ba02a271f92d802699631
Caroline Dequé <carolinedeque@gmail.com>
2023-04-30 14:48:45 +0200

    Ajout modifications client-detail

--- src/app/client-container/client-detail/client-detail.component.ts
+++ src/app/client-container/client-detail/client-detail.component.ts
@@ -10,42 +10,20 @@ import { ListClientsConseillerService } from 'src/app/services/list-clients-cons
 })
 export class ClientDetailComponent {
   @Input() clientSelected!: Client;
-<<<<<<< HEAD
-  selectedClient!: Client;
-  
-
-  constructor(
-    private clientService: ListClientsConseillerService,
-    private route: ActivatedRoute
-  ) {}
-
-  ngOnInit() {
-    const advisorId = this.route.snapshot.params['id'];
-    console.log(this.clientService.getListClientByAdvisorId(advisorId));
-  }
-  
-
-  recupererSelectedClient(client: Client) {
-    this.selectedClient = client;
-=======
   advisorId: string | undefined;
-  // selectedClient!: Client;
+  
 
   constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOninit() {
     this.advisorId = this.route.snapshot.params['id'];
   }
-  // recupererSelectedClient(client: Client) {
-  //   this.selectedClient = client;
-  // }
 
   goToEditClient(clientId: number) {
     this.advisorId = this.route.snapshot.params['id'];
     this.router.navigateByUrl(
       `/advisor/${this.advisorId}/updateClient/${clientId}`
     );
->>>>>>> df3b7459dcbcc443864cf093ecc52c79988cc550
   }
 
 }
